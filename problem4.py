3ada0b13fb42fa9e8f35c2923f6803351df3af7c
SchmetterlingIII <99736344+SchmetterlingIII@users.noreply.github.com>
2025-01-03 16:56:26 +0000

    completed problem4

--- problem4.py
+++ problem4.py
@@ -1,18 +1,28 @@
-number = 102941
-is_palindrome = True 
-numberList = [int(i) for i in str(number)]
-leng = len(numberList)
+'''Find the largest palindrome made from the product of two 3-digit numbers.'''
 
-print(f"Number List: {numberList}")  # Debugging: Show the numberList
-
-for i in range(leng // 2):
-    print(f"Comparing {numberList[i]} and {numberList[leng - i - 1]}")  # Debugging
-    if numberList[i] != numberList[leng - i - 1]:
-        is_palindrome = False
-        print("Mismatch found. Breaking loop.")  # Debugging
-        break
-
-if is_palindrome:
-    print("The number is a palindrome")
-else:
-    print("Not a palindrome")
+'''Finding products of the largest 3-digit numbers'''
+def corr():
+    input1 = 999
+    input2 = 999
+    correct = True
+    while correct:
+        number = input1 * input2
+        if not is_palindrome(number):
+            if input2 > 899: 
+                input2 -= 1
+            else: 
+                input1 -= 1
+                input2 = 999
+        else:
+            correct = False
+            break
+    print(f'The largest palindrome is: {number}, from {input1} * {input2}')
+  
+'''Verifying whether a number is a palindrome'''
+def is_palindrome(number):
+    numberList = [int(i) for i in str(number)]
+    leng = len(numberList)
+    for i in range(leng // 2):
+        if numberList[i] != numberList[leng - i - 1]:
+            return False
+    return True
